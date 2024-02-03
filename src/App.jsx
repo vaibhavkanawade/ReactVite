cca9cc188c006344ffc0d1105ab9ec74081455c9
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-02-04 00:54:59 +0530

    {} use

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Button from "./Button";
+// import Button from "./Button";
+import Hello from "./Hello";
 //in only declare export is 
 // import {Button} from "./Button";
 function App() {
   return <div>
     <h1>hi</h1>
-    <Button> </Button>
+    {/* <Button> </Button> */}
+    <Hello></Hello>
   </div>
 }
 export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello() {
+    let student = "vaibhav";
+    let No = () => {
+        return "Nice";
+    }
+    return <p>
+        Hello welcome  {student} {No()}
+    </p>
+}
+
+export default Hello;
